7931c2032ae263a8e166258d7c9e37b732449543
David Clark <dclark@greenchef.com>
2018-04-07 16:39:14 -0600

    Added CORS allowances.

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(express.json());
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/messages', (req, res) => {
